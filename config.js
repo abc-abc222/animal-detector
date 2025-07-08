399c2720509c2b95c353222fd9c831c4428f4cee
abc-abc222 <3339ra1@gmail.com>
2025-07-08 23:15:08 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // あなたのGoogle Apps ScriptのURLに置き換えてください
-const GAS_URL = "https://script.google.com/macros/s/AKfycbygHllFD1VuOhqlhRlPN2Al_9n4721q_zmWtC9NAk-y5AbwAlKwg0E5cuptVXIwJYL3iA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxn8rjJIxsSUutLFI6gxG1TLzU0pFAc-OJTMRh4DhkaK5hQThWE3tD_oM2_CFhp8lirQw/exec";
